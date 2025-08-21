666f72380a1e02753905c7d304e92d96019cb1e7
zyc-2024 <61992011@qq.com>
2025-08-21 15:02:03 +0800

    Added a brand new homepage

---
+++ index.js
@@ -0,0 +1,32 @@
+function read(f) {
+	return $.ajax({
+		url:
+			"https://gitee.com/api/v5/repos/zyc-2024/chat/raw/" +
+			f +
+			"?access_token=19f7b43872c256d52d1bc71cbd2d0ffa",
+		crossDomain: true,
+		contentType: "application/json;charset=UTF-8",
+	}).then(function (response) {
+		console.log(response);
+		return JSON.parse(response);
+	});
+}
+function write(f, d) {
+	return $.ajax({
+		url:
+			"https://gitee.com/api/v5/repos/zyc-2024/chat/contents/" +
+			encodeURIComponent(f),
+		type: "PUT",
+		data: JSON.stringify({
+			access_token: "19f7b43872c256d52d1bc71cbd2d0ffa",
+			message: "update",
+			content: Base64.encode(JSON.stringify(d)),
+			sha: d.sha,
+		}),
+		crossDomain: true,
+		contentType: "application/json;charset=UTF-8",
+	}).then(function (response) {
+		console.log(response);
+		return response;
+	});
+}
